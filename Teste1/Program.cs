1c6e2c35562f853a2061ddca3c0b884d24697ecc
Jose Pinheiro <jhopinheiro@yahoo.com.br>
2020-06-10 16:53:29 -0300

    Acrescentado a mensagem desenjando um bom dia

--- Teste1/Program.cs
+++ Teste1/Program.cs
@@ -7,8 +7,9 @@ namespace Teste1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine( "bom dia");
-            Console.WriteLine("Como vai vc hoje?");
+            Console.WriteLine( "Tenha um bom dia" );
+
+
         }
     }
 }
